3ae8f04e37448873a0506ca473603c51c8d1deb8
Hatim <7atim1000@gmail.com>
2025-09-14 01:10:34 +0400

    Update useLoadData.js

--- Erp-frontend/src/hooks/useLoadData.js
+++ Erp-frontend/src/hooks/useLoadData.js
@@ -1,39 +1,43 @@
-import { useEffect, useState } from "react";
-import { getUserData } from "../https";
-import { removeUser, setUser } from "../redux/slices/userSlice";
 import { useDispatch } from "react-redux";
+import { getUserData } from "../https";
+import { useEffect, useState } from 'react';
+import { removeUser, setUser } from "../redux/slices/userSlice";
 import { useNavigate } from "react-router-dom";
 
 const useLoadData = () => {
-  const dispatch = useDispatch();
-  const navigate = useNavigate();
-  const [isLoading, setIsLoading] = useState(true);
+    
+    const dispatch = useDispatch();
+    const navigate = useNavigate();
+    const [ isLoading, setIsLoading ] = useState(true);
 
-  useEffect(() => {
-    const fetchUser = async () => {
-        try {
-            const { data } = await getUserData();
-            console.log(data);
-
-            const { _id, employeeNo, name, password, email, phone, role, department, userJob, jobNo, jobDate, userSalary, image } = data.data;
-            dispatch(setUser({ _id, employeeNo, name, password, email, phone, role, department, userJob, jobNo, jobDate,  userSalary, image }));
-        
-        
-        } catch (error) {
-            dispatch(removeUser());
-            navigate('/auth');
-            console.log(error);
-        
-        } finally {
-          setIsLoading(false);
+      useEffect(() => {
+        const fetchUser = async () => {
+            try {
+                const { data } = await getUserData();
+                
+                // Check if the response is actually successful
+                if (data?.success) {
+                    const {_id, name, email, phone, role } = data.data;
+                    dispatch(setUser({ _id, name, email, phone, role }));
+                } else {
+                    throw new Error("No user data");
+                }
+            } catch (error) {
+                // Clear any potential lingering cookies
+                document.cookie = 'accessToken=; Path=/; Expires=Thu, 01 Jan 1970 00:00:01 GMT;';
+                dispatch(removeUser());
+                navigate('/auth');
+            } finally {
+                setIsLoading(false);
+            }
         }
-    }
 
-    fetchUser();
-  }, [dispatch, navigate]);
+        fetchUser();
+    }, [dispatch, navigate]);
 
-  return isLoading;
-};
+    return isLoading;
+    
+}
 
 
 export default useLoadData;
